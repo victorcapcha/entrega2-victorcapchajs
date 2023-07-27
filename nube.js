2e1d68ee6d750ebc6114c6319ee406164bc22995
victor <victor_14x_02s@hotmail.com>
2023-07-27 00:55:35 -0300

    quitando un codigo

--- nube.js
+++ nube.js
@@ -80,50 +80,7 @@ let bebidas = [
     "tamaño": "2,25 lt",
     "stock": 20
   },
-
-  // Agrega aquí más tipos de bebidas con sus respectivos datos
-  // ...
 ];
 
-// Variable para almacenar el resultado de operaciones
 let resultado;
 
-
-// //Creamos un array de 15 tipos de bebidas, cada una con sus atributos
-// bebidas = [
-//     {
-//         "nombre": "Coca-Cola",
-//         "precio": 2.5,
-//         "sabor": "Cola",
-//         "marca": "Coca-Cola Company",
-//         "tamaño": "500ml",
-//         "stock": 50
-//     },
-//     {
-//         "nombre": "Agua Mineral",
-//         "precio": 1.0,
-//         "sabor": "Neutral",
-//         "marca": "Manantial",
-//         "tamaño": "1 litro",
-//         "stock": 100
-//     },
-//     {
-//         "nombre": "manaos",
-//         "precio": 3.0,
-//         "sabor": "Naranja",
-//         "marca": "Delicioso",
-//         "tamaño": "300ml",
-//         "stock": 20
-//     },
-//     //Agrega aquí más tipos de bebidas con sus respectivos datos
-    
-// ]
-
-// //Acceso a los datos de una bebida específica
-// bebida_1 = bebidas[0]
-// nombre_bebida_1 = bebida_1["nombre"]
-// precio_bebida_1 = bebida_1["precio"]
-// sabor_bebida_1 = bebida_1["sabor"]
-// marca_bebida_1 = bebida_1["marca"]
-// tamaño_bebida_1 = bebida_1["tamaño"]
-// stock_bebida_1 = bebida_1["stock"]
